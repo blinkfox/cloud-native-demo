38b2ea6ff3bfec7f82269d5de22929f4d78ae20b
chenjiayin02 <chenjiayin02@meituan.com>
2022-09-04 22:19:02 +0800

    新增了 SpringBoot 的原生示例

---
+++ springboot-native-demo/src/main/java/com/blinkfox/demo/SpringbootNativeDemoApplication.java
@@ -0,0 +1,13 @@
+package com.blinkfox.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootNativeDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootNativeDemoApplication.class, args);
+	}
+
+}
